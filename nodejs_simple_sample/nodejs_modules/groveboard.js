dc90332b6359810b0f4a84f7e43c9432f637d4ce
Jessica Wong <jessicawong@Jessicas-MacBook-Pro.local>
2017-06-20 14:24:04 -0700

    Created Grove Board temperature and Humudity sensor module

---
+++ nodejs_simple_sample/nodejs_modules/groveboard.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let GrovePi = require('node-grovepi').GrovePi;
+let Commands = GrovePi.commands;
+let Board = GrovePi.board;
+let DHTSensor = GrovePi.sensors.DHTDigitalSensor;
+
+module.exports = {
+    broker: null,
+    configuration: null,
+    board: null,
+
+    create: function (broker, configuration) {
+        this.broker = broker;
+        this.configuration = configuration;
+        this.board = new Board({
+            debug: true,
+            onError: function(err) {
+                console.log(`Something wrong with board: ${err} `);
+            },
+            onInit: function(res) {
+                if (res) {
+                    console.log(`GrovePi version: ${board.version()}`);
+                }
+                var TempAndHumiditySensor = new DHTSensor(4, 0, 'f');
+                console.log('Temperature and Humidity sensor activated');
+                TempAndHumiditySensor.stream(4000, function(res) {
+                    this.broker.publish({
+                        properties: {
+                            'source': 'groveboard'
+                        },
+                        content: res
+                    })
+                });
+            }
+        });
+
+        return true;
+    },
+
+    start: function () {
+        board.init();
+    },
+
+    receive: function(message) {
+    },
+
+    destroy: function() {
+        console.log('board destroy');
+        this.board.close();
+    }
+};
